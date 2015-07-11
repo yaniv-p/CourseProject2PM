73fe816baa41f4013992b5d8e59d7e91c27a9fc5
Yaniv Perets <yaniv.perets@gmail.com>
2015-07-11 22:54:15 +0300

    add

--- Plot1.R
+++ Plot1.R
@@ -1,1 +1,11 @@
-# Plot1
+library(dplyr)
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+t1<-merge(NEI,SCC)
+
+#open a png device
+png("plot1.png", width=480, height=480)
+
+p1<-summarise(group_by(t1,year),total=sum(Emissions))
+plot(p1$year,p1$total,type = 'b',main = "PM2.5 emitted, in tons over time",xlab = "Year",ylab = "Emissions")
+dev.off()
