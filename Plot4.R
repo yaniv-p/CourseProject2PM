6c13f6488a862f974ef725efef19e60f5c85759c
yaniv-p <yaniv.perets@gmail.com>
2015-07-15 15:13:30 +0300

    updated

--- Plot4.R
+++ Plot4.R
@@ -10,12 +10,13 @@ if(!file.exists("data")) {
 
 NEI <- readRDS("data\\summarySCC_PM25.rds")
 SCC <- readRDS("data\\Source_Classification_Code.rds")
-NEI<-merge(NEI,SCC)
+#NEI<-merge(NEI,SCC)
 
 #open a png device
 png("plot4.png", width=800, height=500)
-
-filter(SCC, grepl(" Coal ",Short.Name))$Short.Name
-p<-summarise(group_by(t,year,type),total=sum(Emissions))
-qplot(year,total,data=p,facets = . ~ type, geom = c("point","smooth"),method="lm",ylab ="Emissions(Tons)",main = "Baltimore City, Maryland PM2.5 emitted, over time per Type")
+i<-grep(pattern = "coal",x = SCC$Short.Name,ignore.case = TRUE ) #get the rows number we have "coal" in Short.Name
+c<-as.character(SCC$SCC[i]) #conver dom factor to charcter
+NEI1<-NEI[NEI$SCC %in% c,]
+p<-summarise(group_by(NEI1,year),total=sum(Emissions))
+barplot(p$total/1000,names.arg=p$year,main = "Coal combustion-related PM2.5 emitted,over time",xlab = "Year",ylab = "Emissions(Thouesnds of Tons)")
 dev.off()
